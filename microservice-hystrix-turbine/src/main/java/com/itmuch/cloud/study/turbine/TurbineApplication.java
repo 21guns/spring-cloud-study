3f2965855726bf18554ec7a435f4ab2337625de9
jliu <534836006>
2017-05-17 10:42:01 +0800

    issue: 1.upgrade spring cloud and boot version 2.change eureka to consul

--- microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/study/turbine/TurbineApplication.java
+++ microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/study/turbine/TurbineApplication.java
@@ -2,14 +2,17 @@ package com.itmuch.cloud.study.turbine;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * 通过@EnableTurbine接口，激活对Turbine的支持。
+ * 当使用consul时，需要使用@EnableDiscoveryClient 参看：https://github.com/spring-cloud/spring-cloud-consul/blob/master/docs/src/main/asciidoc/spring-cloud-consul.adoc#circuit-breaker-with-hystrix
  * @author eacdy
  */
 @SpringBootApplication
 @EnableTurbine
+@EnableDiscoveryClient
 public class TurbineApplication {
   public static void main(String[] args) {
     new SpringApplicationBuilder(TurbineApplication.class).web(true).run(args);
